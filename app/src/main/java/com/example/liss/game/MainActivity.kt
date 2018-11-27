1fa9def489cf3cdf3a50cfc4636ba08b1a2e13d7
Unknown <lfernandez@iesfbmoll.org>
2018-11-27 21:07:46 +0100

    Add basic slash animation

--- app/src/main/java/com/example/liss/game/MainActivity.kt
+++ app/src/main/java/com/example/liss/game/MainActivity.kt
@@ -4,15 +4,32 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import java.util.*
 import kotlin.concurrent.schedule
 
+
 class MainActivity : GameActivity() {
+    private lateinit var logo1 : TextView
+    private lateinit var logo2 : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        logo1 = findViewById<View>(R.id.TextViewTopTitle) as TextView
+        logo2 = findViewById<View>(R.id.TextViewBottomTitle) as TextView
+
+        applyAnimation()
         checkUserName()
+    }
+
+    private fun applyAnimation() {
+        val fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        logo1.startAnimation(fade1)
+        val fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2)
+        logo2.startAnimation(fade2)
     }
 
     private fun checkUserName() {
@@ -33,5 +50,12 @@ class MainActivity : GameActivity() {
     private fun openMenu() {
         val intent = Intent(this, MenuActivity::class.java)
         startActivity(intent)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        logo1.clearAnimation()
+        logo2.clearAnimation()
     }
 }
